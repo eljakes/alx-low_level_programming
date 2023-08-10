954d923115d53aa0e68e3c2cf10399087a865147
Elvis Owusu-Sekyere <elvlon@yahoo.com>
2023-08-10 09:48:22 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+
 /**
- * main -to print out output using puts
+ * main - Entry point
  *
- * Return: always (0)
- *
+ * Return: Always 0 (Success)
  */
-int main ()
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
