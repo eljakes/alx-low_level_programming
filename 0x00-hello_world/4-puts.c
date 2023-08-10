1728c8992a76e96259df638917da22e871939e37
Elvis Owusu-Sekyere <elvlon@yahoo.com>
2023-08-10 09:31:26 -0700

    using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -to print out output using puts
+ *
+ * Return: always (0)
+ *
+ */
+int main ()
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+        return (0);
+}
