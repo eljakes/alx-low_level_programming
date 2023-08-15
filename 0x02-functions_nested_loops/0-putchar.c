3a65eb96a0112941ec1f42ca9fcc932243157dfb
Elvis Owusu-Sekyere <elvlon@yahoo.com>
2023-08-15 08:09:35 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,4 @@
-#include <unistd.h>
-/**
- * _putchar - Writes a character to the standard output (stdout)
- * @c: The character to be written
- *
- * Return: On success, returns the number of characters written.
- * On error, returns -1 and sets errno appropriately.
- */
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
+#include "holberton.h"
 /**
  * main - Entry point of the program
  *
@@ -19,14 +8,14 @@ return (write(1, &c, 1));
  */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
