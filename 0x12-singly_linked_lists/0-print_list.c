d63b561ed6142bf7df22c47313eb0888d213aa37
Elvis Owusu-Sekyere <elvlon@yahoo.com>
2023-09-22 07:42:06 -0700

    Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* print_list - print all the elements
+* @h: pointer to the head of the singly linked list
+*
+* Return: number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+size_t count = 0;
+
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%lu] %s\n", (unsigned long int)h->len, h->str);
+h = h->next;
+count++;
+}
+return (count);
+}
